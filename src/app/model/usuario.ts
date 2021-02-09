743d75917cde0193d8384f7badbb3d9effff3c97
carlos.dasilva <carlos.dasilva@goias.gov.br>
2021-02-09 11:28:01 -0300

    adicionado métodos gets na classe model usuario.ts

--- src/app/model/usuario.ts
+++ src/app/model/usuario.ts
@@ -1,8 +1,28 @@
 export class Usuario {
-  private id: string;
+
+  private id: number;
   private nome: string;
   private login: string;
   private cpf: string;
   private cep: string;
 
+  public getId(): number{
+    return this.id;
+  }
+
+  public getNome(): string{
+    return this.nome;
+  }
+
+  public getLogin(): string{
+    return this.login;
+  }
+
+  public getCpf(): string{
+    return this.cpf;
+  }
+
+  public getCep(): string{
+    return this.cep;
+  }
 }
